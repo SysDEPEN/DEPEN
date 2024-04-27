247dd6b5735a5bad7b7f5242902410e511050e78
guiphilippsen <dorimedev2021@gmail.com>
2024-04-27 14:43:48 -0300

    file:database.sql

--- database/database.sql
+++ database/database.sql
@@ -1,7 +1,11 @@
 CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
     nome varchar(256) NOT NULL,
+    documento varchar(14),
+    email varchar(256),
     senha varchar(256) NOT NULL,
+    created_at datetime NOT NULL,
+    updated_at datetime NOT NULL
     role SMALLINT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS documentos();
