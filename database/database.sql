8f97f2163ca56b8f38ee93ad45a3127de352ea21
guiphilippsen <dorimedev2021@gmail.com>
2024-04-28 17:19:36 -0300

    edit: database.sql

--- database/database.sql
+++ database/database.sql
@@ -4,10 +4,38 @@ CREATE TABLE IF NOT EXISTS users(
     documento varchar(14),
     email varchar(256),
     senha varchar(256) NOT NULL,
-    created_at datetime NOT NULL,
-    updated_at datetime NOT NULL
-    role SMALLINT NOT NULL
+    role SMALLINT NOT NULL,
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL
 );
-CREATE TABLE IF NOT EXISTS documentos();
-CREATE TABLE IF NOT EXISTS endereco();
-CREATE TABLE IF NOT EXISTS protocolos();
+CREATE TABLE IF NOT EXISTS documentos(
+    id SERIAL PRIMARY KEY,
+    documentos TEXT[],
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL,
+    id_user SERIAL NOT NULL,
+
+    FOREIGN KEY (id_user) REFERENCES users(id)
+);
+CREATE TABLE IF NOT EXISTS endereco(
+    id SERIAL PRIMARY KEY,
+    cep varchar(9) NOT NULL,
+    pais varchar(256) NOT NULL,
+    UF varchar(2) NOT NULL,
+    cidade varchar(256) NOT NULL,
+    bairro varchar(256) NOT NULL,
+    rua varchar(256) NOT NULL,
+    numero_casa varchar(256) NOT NULL,
+    id_user SERIAL NOT NULL,
+
+    FOREIGN KEY (id_user) REFERENCES users(id)
+);
+CREATE TABLE IF NOT EXISTS protocolos(
+    id SERIAL PRIMARY KEY,
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL,
+    id_user SERIAL NOT NULL,
+    id_user_editor SERIAL,
+
+    FOREIGN KEY (id_user) REFERENCES users(id) 
+);
