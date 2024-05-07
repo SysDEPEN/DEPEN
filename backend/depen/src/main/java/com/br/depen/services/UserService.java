33c1b3fbcd674e41e8c1acc996e74c780d0b94b0
Kauã Werle <kauawerle19@gmail.com.br>
2024-05-07 19:09:33 -0300

    fix backend

--- backend/depen/src/main/java/com/br/depen/services/UserService.java
+++ backend/depen/src/main/java/com/br/depen/services/UserService.java
@@ -1,10 +1,12 @@
 package com.br.depen.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.br.depen.entities.UserEntity;
 import com.br.depen.repositories.UsersRepository;
-import com.sun.tools.javac.util.List;
 
 @Service
 public class UserService {
@@ -12,7 +14,9 @@ public class UserService {
 	@Autowired
 	private UsersRepository repository;
 	
-	public List<UserEn>
+	public List<UserEntity> FindAll() {
+		return repository.findAll();
+	}
 
 	
 }
