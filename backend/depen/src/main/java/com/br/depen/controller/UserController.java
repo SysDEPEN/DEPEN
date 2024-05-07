dd0e2777de688809c0e15384ec809932ac39febd
Kauã Werle <kauawerle19@gmail.com.br>
2024-05-07 19:17:53 -0300

    create controller

---
+++ backend/depen/src/main/java/com/br/depen/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.br.depen.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.depen.entities.UserEntity;
+import com.br.depen.services.UserService;
+
+@RestController
+@RequestMapping
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping
+	public ResponseEntity<List<UserEntity>> FindAll(){
+		return ResponseEntity.ok(userService.FindAll());
+	}
+}
