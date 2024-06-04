c5a63147eae447732ae56a0817f9617e9caaafc0
guiphilippsen <dorimedev2021@gmail.com>
2024-06-04 00:32:29 -0300

    rebuild backend

--- database/database.sql
+++ database/database.sql
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS tb_login(
     updated_at timestamp NOT NULL,
 	id_user SERIAL,
 	id_admin SERIAL,
-	
+
 	FOREIGN KEY(id_user) REFERENCES tb_user(id),
 	FOREIGN KEY(id_admin) REFERENCES tb_admin(id)
 );
@@ -43,8 +43,8 @@ CREATE TABLE IF NOT EXISTS tb_subject(
 	id_inmost_visit SERIAL NOT NULL,
 	created_at timestamp NOT NULL,
 	updated_at timestamp NOT NULL,
-	
-	FOREIGN KEY(id_user) REFERENCES tb_user(id)
+
+	FOREIGN KEY(id_user) REFERENCES tb_user(id),
 	FOREIGN KEY(id_inmost_visit) REFERENCES tb_subject_inmost_visit(id)
 );
 
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS tb_subject_inmost_visit(
 	id_user SERIAL NOT NULL,
 	created_at timestamp NOT NULL,
 	updated_at timestamp NOT NULL,
-	FOREIGN KEY(id_user) REFERENCES tb_user(id);
+	FOREIGN KEY(id_user) REFERENCES tb_user(id)
 );
 
 CREATE TABLE IF NOT EXISTS tb_documents(
@@ -89,44 +89,6 @@ CREATE TABLE IF NOT EXISTS protocolos(
     id_user SERIAL NOT NULL,
     id_user_editor SERIAL,
 
-    FOREIGN KEY (id_user) REFERENCES tb_user(id) 
-	FOREIGN KEY (id_user_editor) REFERENCES tb_admin(id) 
+    FOREIGN KEY (id_user) REFERENCES tb_user(id),
+	FOREIGN KEY (id_user_editor) REFERENCES tb_admin(id)
 );
-
-
-SELECT * from tb_user
-INSERT INTO tb_user (nome, documento, email, sexo, data_nasc, senha, role, created_at, updated_at)
-VALUES ('João Silva', '123.456.789-00', 'joao@example.com', 'Masculino', '2001-10-05', 'senha123', 0, NOW(), NOW()),
-       ('Maria Souza', '987.654.321-00', 'maria@example.com','Feminino', '2003-12-20', 'senha456', 0, NOW(), NOW()),
-       ('Maria Souza', '987.654.321-00', 'maria@example.com', 'Feminino', '2003-12-20', 'senha456', 0, NOW(), NOW());
-
-SELECT * FROM tb_user
-INSERT INTO tb_address (cep, pais, UF, cidade, bairro, rua, numero_casa, id_user)
-VALUES ('12345-678', 'Brasil', 'PR', 'Foz do Iguaçu', 'Centro', 'Rua A', '123', 1),
-       ('54321-876', 'Brasil', 'PR', 'Foz do Iguaçu', 'Morumbi', 'Av. B', '456', 2),
-       ('54321-876', 'Brasil', 'PR', 'Foz do Iguaçu', 'Morumbi', 'Av. C', '236', 3);
-
-
-CREATE OR REPLACE FUNCTION include_value_protocol()
-RETURNS TRIGGER AS $$
-BEGIN
-    INSERT INTO tb_protocols(created_at, updated_at, id_user)
-	VALUES(NOW(), NOW(), NEW.id_user);
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER insert_protocols_value
-AFTER INSERT
-ON tb_protocols
-FOR EACH ROW
-WHEN (pg_trigger_depth() < 1)
-EXECUTE FUNCTION include_value_protocol();
-
-INSERT INTO tb_protocols(created_at, updated_at, id_user)
-	VALUES(NOW(), NOW(), 1);
-	
-	SELECT * FROM tb_protocols
-
-DROP TRIGGER insert_protocols_value ON tb_protocols;
-DROP FUNCTION include_value_protocol;
