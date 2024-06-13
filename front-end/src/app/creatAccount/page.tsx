a4a131f6ebbbb280293fbe034ccac8e51b041dcf
Kauã Werle <kauawerle19@gmail.com.br>
2024-06-13 20:01:57 -0300

    installed axios

--- front-end/src/app/creatAccount/page.tsx
+++ front-end/src/app/creatAccount/page.tsx
@@ -1,12 +1,49 @@
+"use client";
 import FooterComponent from "@/components/Footer/footer";
 import HeaderComponent from "@/components/Header/header";
+import { UserProps } from "@/interfaces/userProps";
+import api from "@/services/api";
+import { FormEvent, useState } from "react";
 
-export default function SendDocument() {
+export default function createAccount() {
+  // const navigate = useNavigate();
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
+  const [gender, setGender] = useState("");
+  const [document, setDocument] = useState("");
+  const [dateBorn, setDateBorn] = useState("");
+
+  async function handleRegister(e: FormEvent) {
+    e.preventDefault();
+
+    const data: UserProps = {
+      name,
+      email,
+      password,
+      document,
+      gender,
+      dateBorn,
+      role: 0,
+    };
+
+    try {
+      await api.post("user", data);
+      alert("Cadastro realizado com sucesso!!");
+      // navigate(`/login`);
+    } catch (err: any) {
+      setErrorMessage(err);
+    }
+  }
+
   return (
     <section className="">
       <HeaderComponent />
       <section className="w-full flex h-[45vw] justify-center items-center bg-white">
-        <div className="
+        <div
+          className="
           flex
           bg-[#DAE2E8]
           shadow-lg
@@ -22,34 +59,35 @@ export default function SendDocument() {
           mb-2
           h-xl
           w-full
-          max-w-96">
-
-
-          <label
-            className="block text-gray-700 font-bold m-2 py-1 " >
+          max-w-96"
+        >
+          <label className="block text-gray-700 font-bold m-2 py-1 ">
             Cadastras sua conta
           </label>
 
-
-          <form action="" className="
+          <form
+            action=""
+            onSubmit={handleRegister}
+            className="
           flex 
           justify-center 
           items-center
-          flex-col">
-
-
+          flex-col"
+          >
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline m-4 w-64"
               type="text"
               placeholder="Nome Completo"
+              onChange={(e) => setName(e.target.value)}
               id=""
               name=""
             />
 
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline m-4 w-64"
-              type="email"
+              type="text"
               placeholder="CPF/RNE"
+              onChange={(e) => setDocument(e.target.value)}
               id=""
               name=""
             />
@@ -58,6 +96,7 @@ export default function SendDocument() {
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline m-4 w-64"
               type="email"
               placeholder="Seu email"
+              onChange={(e) => setEmail(e.target.value)}
               id=""
               name=""
             />
@@ -67,11 +106,27 @@ export default function SendDocument() {
               id=""
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline m-4 w-64"
             >
-              <option value="" disabled selected>Selecione seu gênero</option>
-              <option value="">opc1</option>
-              <option value="">opc2</option>
-              <option value="">opc3</option>
-
+              <option value="DEFAULT" disabled selected>
+                Selecione seu gênero
+              </option>
+              <option
+                onChange={(e) => setGender(e.currentTarget.value)}
+                value="masculino"
+              >
+                Masculino
+              </option>
+              <option
+                onChange={(e) => setGender(e.currentTarget.value)}
+                value="feminino"
+              >
+                Feminino
+              </option>
+              <option
+                onChange={(e) => setGender(e.currentTarget.value)}
+                value="outros"
+              >
+                Outros
+              </option>
             </select>
 
             <input
@@ -90,32 +145,19 @@ export default function SendDocument() {
               name=""
             />
 
-            <button className="flex items-center shadow-lg justify-center py-4 bg-[#1348D0] rounded-md w-[177px] h-[39px] m-4 " >
+            <button className="flex items-center shadow-lg justify-center py-4 bg-[#1348D0] rounded-md w-[177px] h-[39px] m-4 ">
               Cadastrar
             </button>
 
-
-
-            <label htmlFor=""
-              className=" text-gray-700  py-1"
-            >Já possui uma conta?
-              <a href="/login" className="text-blue-700"
-              > Acesse sua conta.
+            <label htmlFor="" className=" text-gray-700  py-1">
+              Já possui uma conta?
+              <a href="/login" className="text-blue-700">
+                {" "}
+                Acesse sua conta.
               </a>
             </label>
-
-
-
-
           </form>
-
-
-
-
         </div>
-
-
-
       </section>
       <FooterComponent />
     </section>
